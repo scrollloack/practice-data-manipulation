0e9dab926b015780b6b67e08e0a23b305037534e
scrollloack <band.archie13@gmail.com>
2025-07-29 21:49:56 +0800

    Solution to third problem

---
+++ binV2/third_problem.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+n = ARGV[1].to_i
+n = 1 if n.zero?
+
+input_data = [
+  { "name": "Apples", "quantity_sold": 120 },
+  { "name": "Oranges", "quantity_sold": 75 },
+  { "name": "Bananas", "quantity_sold": 200 }
+]
+
+sorted_data = input_data.sort_by { |data| data[:quantity_sold] }.reverse!
+
+top_products = sorted_data.take(n)
+
+puts JSON.pretty_generate({ data: top_products })
+
+print 'hello'

80be86d2d00d7f05b97e122f5b9f2877ca064106
scrollloack <band.archie13@gmail.com>
2025-08-03 11:00:39 +0800

    Solution to fifth problem

---
+++ binV5/fifth_problem.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+input_data = {
+  "users": [
+    { "id": 1, "name": "Alice" },
+    { "id": 2, "name": "Bob" }
+  ],
+  "posts": [
+    { "id": 101, "user_id": 1, "title": "Hello" },
+    { "id": 102, "user_id": 1, "title": "World" },
+    { "id": 103, "user_id": 2, "title": "Ruby Rocks" }
+  ]
+}
+
+users = input_data[:users]
+posts = input_data[:posts].group_by { |data| data[:user_id] }
+
+user_posts = users.map do |user|
+  user_id = user[:id]
+
+  {
+    id: user_id,
+    name: user[:name],
+    posts: (posts[user_id] || []).map { |post| { id: post[:id], title: post[:title] } }
+  }
+end
+
+print JSON.pretty_generate({ data: user_posts })

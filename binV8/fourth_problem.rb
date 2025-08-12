52f0f7f4d12e2526606f17fe85de0e112a38a3be
scrollloack <band.archie13@gmail.com>
2025-08-12 21:29:38 +0800

    Solution to fourth problem in ruby and javascript

---
+++ binV8/fourth_problem.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+customers = [
+  { "name": "john doe", "email": "john@example.com" },
+  { "name": "John Doe", "email": "JOHN@example.com" },
+  { "name": "jane smith", "email": "jane@example.com" }
+]
+
+groupedCustomers = customers.group_by { |data| [ data[:name].downcase, data[:email].downcase ] }
+
+duplicatedCustomers = groupedCustomers.select do |k, v|
+  v.size > 1
+end.values.flatten
+
+puts JSON.pretty_generate({ data: duplicatedCustomers })

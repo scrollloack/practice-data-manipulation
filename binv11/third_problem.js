45af48dbc527173876904d71f17a6e4ab68bd387
scrollloack <band.archie13@gmail.com>
2026-01-22 00:51:07 +0800

    Solution to seven problems in javascript

---
+++ binv11/third_problem.js
@@ -0,0 +1,15 @@
+const n = Number(process.argv[3]) || 2;
+
+const products = [
+  { name: "Apples", quantity_sold: 120 },
+  { name: "Oranges", quantity_sold: 75 },
+  { name: "Bananas", quantity_sold: 200 },
+];
+
+const sortedProducts = products.sort((a, b) => {
+  return b.quantity_sold - a.quantity_sold;
+});
+
+const highestSoldProducts = sortedProducts.slice(0, n);
+
+console.log(JSON.stringify({ data: highestSoldProducts }, null, 2));

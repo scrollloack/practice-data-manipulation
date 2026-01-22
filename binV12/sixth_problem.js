c7fdd412242c15636ca86f936a34b15f6f277dca
scrollloack <band.archie13@gmail.com>
2026-01-22 15:22:48 +0800

    Solution to the sixth problem using javascript

---
+++ binV12/sixth_problem.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const staffs = JSON.parse(fs.readFileSync("input_data/staff.json"));
+
+const highestRatedStaff = staffs.flatMap((staff) => {
+  if (staff?.result?.rating <= 4) return [];
+
+  return {
+    id: staff.id,
+    full_name: staff.full_name,
+    email: staff.email,
+    rating: staff.result.rating,
+  };
+});
+
+console.log(JSON.stringify({ data: highestRatedStaff }, null, 2));

a5113aabff6b5d241d3d9a8efdc249f7e60f880e
scrollloack <band.archie13@gmail.com>
2026-01-27 20:44:39 +0800

    Solution to the eleventh problem using javascript

---
+++ binV17/eleventh_problem.js
@@ -0,0 +1,18 @@
+const StringChallenge = (str1, str2) => {
+  const counts = {};
+
+  for (const char of str1) {
+    counts[char] = (counts[char] ?? 0) + 1;
+  }
+
+  for (const char of str2) {
+    if (!counts[char]) return "false";
+    counts[char]--;
+  }
+
+  return true;
+};
+
+console.log(StringChallenge("wwcdore", "coder")); // "true"
+console.log(StringChallenge("h3llko", "hello")); // "false"
+console.log(StringChallenge("rkqodlw", "world")); // "true"

c297ab7b50e7d14d41889b921dfb34cb5939bd5f
scrollloack <band.archie13@gmail.com>
2026-01-28 17:24:59 +0800

    Solution to the twelfth problem using javascript

---
+++ binV18/twelfth_problem.js
@@ -0,0 +1,18 @@
+const MathChallenge = (num) => {
+  if (num <= 1) return "false";
+  if (num === 2) return "true";
+  if (num % 2 === 0) return "false";
+
+  const limit = Math.sqrt(num);
+  for (let i = 3; i <= limit; i += 2) {
+    if (num % i === 0) {
+      return "false";
+    }
+  }
+
+  return "true";
+};
+
+console.log(MathChallenge(19)); // "true"
+console.log(MathChallenge(65521)); // "true" (Largest prime below 2^16)
+console.log(MathChallenge(1)); // "false"

d7cf1ece4ff75051ec3530ceaeeff524bc4a1445
scrollloack <band.archie13@gmail.com>
2026-01-28 16:27:19 +0800

    Solution to the third problem using javascript

---
+++ binV18/third_problem.js
@@ -0,0 +1,13 @@
+const n = Number(process.argv[3]) || 2;
+
+const products = [
+  { name: "Apples", quantity_sold: 120 },
+  { name: "Oranges", quantity_sold: 75 },
+  { name: "Bananas", quantity_sold: 200 },
+];
+
+const topSoldProducts = products
+  .toSorted((a, b) => b.quantity_sold - a.quantity_sold)
+  .slice(0, n);
+
+console.log(JSON.stringify({ topSoldProducts }, null, 2));

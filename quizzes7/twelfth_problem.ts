4efb3d644c51207e644b2380225d79299b69be8d
scrollloack <band.archie13@gmail.com>
2026-02-16 14:49:58 +0800

    Solution to the twelfth problem using typescript

---
+++ quizzes7/twelfth_problem.ts
@@ -0,0 +1,22 @@
+export {};
+
+const mathChallenge = (num: number): string => {
+  if (!Number.isInteger(num) || num < 2) return "false";
+  if (num === 2) return "true";
+  if (num % 2 === 0) return "false";
+
+  const limit: number = Math.sqrt(num);
+
+  for (let i = 3; i <= limit; i += 2) {
+    if (num % i === 0) return "false";
+  }
+
+  return "true";
+};
+
+console.log(mathChallenge(9)); // "false"
+console.log(mathChallenge(25)); // "false"
+console.log(mathChallenge(65521)); // "true" (Largest prime below 2^16)
+console.log(mathChallenge(19)); // "true"
+console.log(mathChallenge(1)); // "false"
+console.log(mathChallenge(17.5)); // "false"

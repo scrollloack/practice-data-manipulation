72b69089e42e15ee7cdf6c63a1e103caac962b63
scrollloack <band.archie13@gmail.com>
2026-02-16 21:15:20 +0800

    Solution to the seventh problem using typescript

---
+++ quizzes8/seventh_problem.ts
@@ -0,0 +1,73 @@
+export {};
+
+// @ts-ignore
+import fs from "fs";
+
+type Feedback = {
+  comment: string;
+  date: string;
+};
+
+type Result = {
+  rating: number;
+  feedback: Feedback[];
+};
+
+type Staff = {
+  id: number;
+  full_name: string;
+  email: string;
+  result: Result;
+};
+
+type LatestFeedback = Pick<Staff, "email" | "full_name"> & {
+  latest_comment: string;
+};
+
+const staffs: Staff[] = JSON.parse(
+  fs.readFileSync("input_data/staff.json", "utf8"),
+);
+
+const findLatestFeedbacksWithChained = (): LatestFeedback[] => {
+  return staffs.map((staff) => {
+    const latest = staff.result.feedback.toSorted(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+    )[0];
+
+    return {
+      email: staff.email,
+      full_name: staff.full_name,
+      latest_comment: latest?.comment ?? "No available feedback yet.",
+    };
+  });
+};
+
+const findLatestFeedbacksWithLoop = (): LatestFeedback[] => {
+  const latestFeedbacks: LatestFeedback[] = [];
+
+  for (let i = 0; i < staffs.length; i++) {
+    const staff: Staff = staffs[i];
+    const feedbacks: Feedback[] = staff.result.feedback;
+
+    let latest: Feedback | null = null;
+
+    for (const feedback of feedbacks) {
+      if (!latest || new Date(feedback.date) > new Date(latest.date)) {
+        latest = feedback;
+      }
+    }
+
+    latestFeedbacks.push({
+      email: staff.email,
+      full_name: staff.full_name,
+      latest_comment: latest?.comment ?? "No available feedback yet.",
+    });
+  }
+
+  return latestFeedbacks;
+};
+
+console.log(
+  JSON.stringify({ data: findLatestFeedbacksWithChained() }, null, 2),
+);
+console.log(JSON.stringify({ data: findLatestFeedbacksWithLoop() }, null, 2));

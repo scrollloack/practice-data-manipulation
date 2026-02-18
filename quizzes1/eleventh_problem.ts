5ed639000c8578d0e87322960cb09f7d6104ced4
scrollloack <band.archie13@gmail.com>
2026-02-18 11:42:51 +0800

    Solution to the eleventh problem using typescript

---
+++ quizzes1/eleventh_problem.ts
@@ -0,0 +1,21 @@
+export {};
+
+const stringChallenge = (str1: string, str2: string): boolean => {
+  const counts = new Map<string, number>();
+
+  for (const char of str1) {
+    counts.set(char, (counts.get(char) ?? 0) + 1);
+  }
+
+  for (const char of str2) {
+    const currentCount: number = counts.get(char) ?? 0;
+    if (currentCount <= 0) return false;
+    counts.set(char, currentCount - 1);
+  }
+
+  return true;
+};
+
+console.log(stringChallenge("wwcdore", "coder")); // "true"
+console.log(stringChallenge("h3llko", "hello")); // "false"
+console.log(stringChallenge("rkqodlw", "world")); // "true"

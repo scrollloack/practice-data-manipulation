ad0311788cf64cd08d504e75ac32634bcc1b5a30
rchi <band.archie13@gmail.com>
2026-02-28 13:58:23 +0800

    Solution to the 15th problem using typescript

---
+++ quizzes2/15-problem.ts
@@ -0,0 +1,27 @@
+export {};
+
+const rotate = (args: number[], k: number): number[] => {
+    const n = args.length;
+    if (n <= 1) return args;
+
+    k = k % n;
+    if (k === 0) return args;
+
+    const reverse = (arr: number[], start: number, end: number) => {
+        while (start < end) {
+            [arr[start], arr[end]] = [arr[end], arr[start]];
+            start++;
+            end--;
+        }
+    };
+
+    reverse(args, 0, n - 1);
+    reverse(args, 0, k - 1);
+    reverse(args, k, n - 1);
+
+    return args;
+};
+
+const array: number[] = [1, 2, 3, 4, 5, 6, 7];
+console.log(JSON.stringify({ data: rotate(array, 3) }, null, 2));
+// Output: [5, 6, 7, 1, 2, 3, 4]

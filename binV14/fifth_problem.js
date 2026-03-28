0047cb8097461574b2889a7d17096a4816c11ebe
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:17:18 +0800

    Solution to the sixth problem using javascript

---
+++ binV14/fifth_problem.js
@@ -0,0 +1,29 @@
+const data = {
+  users: [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+  ],
+  posts: [
+    { id: 101, user_id: 1, title: "Hello" },
+    { id: 102, user_id: 1, title: "World" },
+    { id: 103, user_id: 2, title: "JS Rocks" },
+  ],
+};
+
+const users = data.users;
+const posts = Object.groupBy(data.posts, (post) => post.user_id);
+
+const userPosts = users.map((user) => {
+  return {
+    id: user.id,
+    name: user.name,
+    posts: (posts[user.id] || []).map((post) => {
+      return {
+        id: post.id,
+        title: post.title,
+      };
+    }),
+  };
+});
+
+console.log(JSON.stringify({ data: userPosts }, null, 2));

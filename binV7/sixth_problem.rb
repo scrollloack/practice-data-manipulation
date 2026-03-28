7a6ee758596435dee8178a8e1c5ecb92a476f6a7
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:16:10 +0800

    Solution to sixth problem

---
+++ binV7/sixth_problem.rb
@@ -0,0 +1,16 @@
+require 'json'
+
+staffs = JSON.parse(File.read('input_data/staff.json'), symbolize_names: true)
+
+highest_rated_staffs = staffs.select { |staff| staff[:result][:rating] > 4 }
+
+filtered_result = highest_rated_staffs.map do |staff|
+  {
+    id: staff[:id],
+    full_name: staff[:full_name],
+    email: staff[:email],
+    rating: staff[:result][:rating]
+  }
+end
+
+print JSON.pretty_generate({ data: filtered_result })

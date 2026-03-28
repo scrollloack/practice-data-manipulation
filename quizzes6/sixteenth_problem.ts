20f5ed6d78666d6f200f2ac9b85fc711dbd6b573
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:21:23 +0800

    Solution to the sixteenth problem using typescript

---
+++ quizzes6/sixteenth_problem.ts
@@ -0,0 +1,21 @@
+export {};
+
+const isPalindrome = (str: string): string => {
+  const chars: string[] = [...str.toLowerCase().replace(/[^a-z0-9]/g, "")];
+
+  let left: number = 0;
+  let right: number = chars.length - 1;
+
+  while (left < right) {
+    if (chars[left] !== chars[right]) return "false";
+    left++;
+    right--;
+  }
+
+  return "true";
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("🚀 racecar 🚀")); // true (Unicode safe!)

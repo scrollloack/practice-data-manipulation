837bc4087f1788ae9ae15633c9e38a562d9154ba
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:20:56 +0800

    Solution to the fourteenth problem using javascript

---
+++ quizzes4/fourteenth_problem.js
@@ -0,0 +1,16 @@
+console.log("Output should be 1, 5, 3, 2, 4");
+(() => {
+  console.log("1: Synchronous start");
+
+  setTimeout(() => {
+    console.log("2: Asynchronous - Timeout 0ms - Macro Task Queue");
+  }, 0);
+
+  Promise.resolve().then(() => console.log("3: Asynchronous - Promise"));
+
+  setTimeout(() => {
+    console.log("4: Asynchronous - Timeout 10ms - Macro Task Queue");
+  }, 10);
+
+  console.log("5: Synchronous end");
+})();

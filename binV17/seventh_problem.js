6f75251ebc6b65d7b80166fd7d89baa11dc5af8e
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:17:40 +0800

    Solution to the seventh problem using javascript

---
+++ binV17/seventh_problem.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const staffs = JSON.parse(fs.readFileSync("input_data/staff.json"));
+
+const latestFeedbacks = staffs.map((staff) => {
+  const latestFeedback = staff?.result?.feedback?.toSorted(
+    (a, b) => new Date(b.date) - new Date(a.date),
+  )[0];
+
+  return {
+    email: staff.email,
+    full_name: staff.full_name,
+    latest_comment: latestFeedback?.comment ?? "No available feedback yet.",
+  };
+});
+
+console.log(JSON.stringify({ latestFeedbacks }, null, 2));

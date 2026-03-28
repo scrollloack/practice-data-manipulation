9471bc252b2e5b5ecc62433fbe2b5b77d1fc146a
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:15:53 +0800

    Solution to third problem

---
+++ binV6/third_problem.rb
@@ -0,0 +1,16 @@
+require 'json'
+
+n = ARGV[1].to_i
+n = 2 if n.zero?
+
+products = [
+  { "name": "Apples", "quantity_sold": 120 },
+  { "name": "Oranges", "quantity_sold": 75 },
+  { "name": "Bananas", "quantity_sold": 200 }
+]
+
+sorted_products = products.sort_by { |data| data[:quantity_sold] }.reverse
+
+most_sold_products = sorted_products.take(n)
+
+print JSON.pretty_generate({ data: most_sold_products })

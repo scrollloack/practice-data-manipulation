922013959f4219793be4255206fcd5a52ca14dca
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:16:34 +0800

    Solution to sixth problem in ruby and javascript

---
+++ binV9/sixth_problem.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+const staffs = JSON.parse(fs.readFileSync("input_data/staff.json", "utf8"));
+
+const highestRatedStaffs = staffs.filter((staff) => staff.result.rating > 4);
+
+const mappedResult = highestRatedStaffs.map((staff) => {
+  return {
+    id: staff.id,
+    full_name: staff.full_name,
+    email: staff.email,
+    rating: staff.result.rating,
+  };
+});
+
+console.log(JSON.stringify({ data: mappedResult }, null, 2));

e3745109467a5cf7009eb0beafcd388d3e8fe063
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:17:28 +0800

    Solution to the fourth problem using javascript

---
+++ binV16/fourth_problem.js
@@ -0,0 +1,16 @@
+const customers = [
+  { name: "john doe", email: "john@example.com" },
+  { name: "John Doe", email: "JOHN@example.com" },
+  { name: "jane smith", email: "jane@example.com" },
+];
+
+const groupedCustomers = Object.groupBy(customers, (customer) => [
+  customer.name.toLowerCase(),
+  customer.email.toLowerCase(),
+]);
+
+const duplicatedCustomer = Object.values(groupedCustomers).flatMap(
+  (customer) => (customer.length > 1 ? customer : []),
+);
+
+console.log(JSON.stringify({ duplicatedCustomer }, null, 2));

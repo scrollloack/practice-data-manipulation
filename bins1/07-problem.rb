9bb76a618f6a8b0ce3a36f116e8afae34d268e8e
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-04-01 14:42:29 +0800

    Solution to the 07th problem using ruby

---
+++ bins1/07-problem.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'date'
+
+staffs = JSON.parse(
+  File.read('input_data/staff.json'),
+  symbolize_names: true
+)
+
+def find_latest_feedbacks_with_chain(staffs)
+  staffs.map do |staff|
+    result = staff[:result]
+    latest = result[:feedback].max_by { |f| Date.parse(f[:date]) }
+
+    staff.slice(:email, :full_name).merge(
+      latest_comment: latest&.dig(:comment) || 'No available feedback yet.'
+    )
+  end
+end
+
+def find_latest_feedbacks_with_loop(staffs)
+  latest_feedbacks = []
+
+  staffs.each do |staff|
+    latest = nil
+
+    staff[:result][:feedback].each do |feedback|
+      if latest.nil? || Date.parse(feedback[:date]) > Date.parse(latest[:date])
+        latest = feedback
+      end
+    end
+
+    latest_feedbacks << staff.slice(:email, :full_name).merge(
+      latest_comment: latest&.dig(:comment) || 'No available feedback yet.'
+    )
+  end
+
+  latest_feedbacks
+end
+
+puts JSON.pretty_generate({ data: find_latest_feedbacks_with_chain(staffs) })
+puts JSON.pretty_generate({ data: find_latest_feedbacks_with_loop(staffs) })

64f1c2d9325ce57a6fef65fc97b46d47463ba70d
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-04-20 16:36:41 +0800

    Solution to the 10th problem using ruby

---
+++ bins6/10-problem.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'json'
+
+def find_first_repeat_char(str)
+  seen = {}
+
+  str.each_char do |char|
+    return char if seen[char]
+    seen[char] = true
+  end
+
+  nil
+end
+
+def find_all_repeat_chars(str)
+  counts = Hash.new(0)
+
+  str.each_char { |c| counts[c] += 1 }
+
+  counts.select do |_, count|
+    count > 1
+  end.keys
+end
+
+puts JSON.pretty_generate({ data: find_first_repeat_char('swiss') })
+puts JSON.pretty_generate({ data: find_first_repeat_char('them') })
+puts JSON.pretty_generate({ data: find_all_repeat_chars('programming') })
